2fc6df030cc9e5297dfedba14d1275adf8ecbeeb
Tetsuo23 <carretmo@gmail.com>
2022-01-06 19:34:03 +0100

    ...

--- front/src/services/post.service.js
+++ front/src/services/post.service.js
@@ -1,38 +1,52 @@
 import http from "../common/http-common";
+import axios from './../../../app/front/react-jwt-auth/src/common/http-common';
 
 
 class PostDataService {
-  getAll() {
-    return http.get("/posts");
-  }
+  // getAll() {
+  //   return http.get("/posts");
+  // }
 
-  get(userId) {
-    return http.get(`/posts/${userId}`);
-  }
+  // get(userId) {
+  //   return http.get(`/posts/${userId}`);
+  // }
 
-  create(data) {
-    return http.post("/posts", data);
-  }
+  // create(data) {
+  //   return http.post("/posts", data);
+  // }
 
-  update(id, data) {
-    return http.put(`/posts/${id}`, data);
-  }
+  // update(id, data) {
+  //   return http.put(`/posts/${id}`, data);
+  // }
 
-  delete(id) {
-    return http.delete(`/posts/${id}`);
-  }
+  // delete(id) {
+  //   return http.delete(`/posts/${id}`);
+  // }
 
-  deleteAll() {
-    return http.delete(`/posts`);
-  }
+  // deleteAll() {
+  //   return http.delete(`/posts`);
+  // }
 
-  findBymois(mois) {
-    return http.get(`/posts?mois=${mois}`);
-  }
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));
+  // findBymois(mois) {
+  //   return http.get(`/posts?mois=${mois}`);
+  // }
+  // getCurrentUser() {
+  //   return JSON.parse(localStorage.getItem('user'));
     
-  }
+  // }
+
+getAll(mois) {
+  return axios
+  .get(API_URL + 'posts', {
+mois
+  })
+  .then(response => {
+    if (response.data.accessToken) {
+      return response.data
+    }
+  })
+}
+
 }
 
 export default new PostDataService();
--- server.js
+++ server.js
@@ -68,6 +68,7 @@ require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 // require("./app/routes/donnees.routes")(app);
 require("./routes/donnee.routes")(app);
+require("./routes/post.routes")(app);
 
 
 // set port, listen for requests
