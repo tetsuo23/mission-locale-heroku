d0045491f9387ca64615ca019497d70187597b44
Tetsuo23 <carretmo@gmail.com>
2022-01-06 14:06:30 +0100

    ...

--- front/src/services/auth.service.js
+++ front/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://mission-locale-heroku.herokuapp.com//api/auth/";
+const API_URL = "https://mission-locale-heroku.herokuapp.com/api/auth/";
 
 class AuthService {
   login(nom, prenom, password, telephone, id) {
