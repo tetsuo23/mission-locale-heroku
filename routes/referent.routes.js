19d9da6ba8799c44f9f1b80c988d0cf3170d9236
Tetsuo23 <carretmo@gmail.com>
2022-01-14 13:38:52 +0100

    correction apparition bouton add apres nouvelle données

---
+++ routes/referent.routes.js
@@ -0,0 +1,24 @@
+module.exports = (app) => {
+    const posts = require("../controllers/referent.controller");
+  
+    const donnees = require("../controllers/donnee.controller");
+  
+    var router = require("express").Router();
+  
+    // Create a new Donnee
+    router.post("/", posts.createPost);
+  
+    // Retrieve a single Tutorial with id
+    // router.get("/:id", posts.findOne);
+  
+    // Retrieve all Tutorials
+    router.get("/:userId/:categorie", posts.findAllUserIdAndCat);
+     router.get("/:userId", posts.findAllUserId);
+     router.get("/", posts.findAll);
+  
+    // ----------------------------------------------------------------------- //
+  
+   
+  
+    app.use("/api/posts", router);
+  };
