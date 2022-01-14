77c17bc2e958908b16e771d8772689871ac215d5
Tetsuo23 <carretmo@gmail.com>
2022-01-14 15:35:17 +0100

    correction apparition bouton add apres nouvelle données

--- controllers/referent.controller.js
+++ controllers/referent.controller.js
@@ -3,7 +3,23 @@ const User = db.user;
 const Post = db.post;
 exports.findAllUserId = (req, res) => {
     const userId = req.params.userId;
-    User.findAll({ where: { userId: userId } })
+    User.findAll()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+  };
+
+  exports.findAll = (req, res) => {
+    const mois = req.body.mois;
+    
+  
+    User.findAll()
       .then(data => {
         res.send(data);
       })
